98249aed851da14c9ad386fb7adbb90813a63d86
Tahir Narimane <narimane.tahir.etu@univ-lille.fr>
2021-04-25 16:29:16 +0200

    tests

--- project/test/testNourir.java
+++ project/test/testNourir.java
@@ -13,25 +13,12 @@ class testNourir {
 
 	@Test
 	public void testExecute() {
-		List<Student> sorted = this.group.alphabeticalOrder();
-		assertTrue(sorted.get(0).getName().compareTo(sorted.get(1).getName()) <= 0);
-		assertTrue(sorted.get(1).getName().compareTo(sorted.get(2).getName()) <= 0);
+		
 	}
 
 	@Test
 	public void testCoutArmee() {
-		// st3 > st1 > st2
-		st1.addMark(Subject.POO, 10);
-		st1.addMark(Subject.BDD1, 12);
-		st2.addMark(Subject.POO, 8);
-		st2.addMark(Subject.BDD1, 10);
-		st3.addMark(Subject.POO, 15);
-		st3.addMark(Subject.BDD1, 15);
-		
-		List<Student> sorted = this.group.averageOrder();
-		assertEquals(st3, sorted.get(0));
-		assertEquals(st1, sorted.get(1));
-		assertEquals(st2, sorted.get(2));
+	
 		
 	}
 
