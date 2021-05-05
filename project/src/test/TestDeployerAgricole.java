5ecf6c6c093559fe3de3c3fd512948547f7b3019
Tahir Narimane <narimane.tahir.etu@univ-lille.fr>
2021-05-05 16:35:02 +0200

    tests

--- project/src/test/TestDeployerAgricole.java
+++ project/src/test/TestDeployerAgricole.java
@@ -21,6 +21,7 @@ class TestDeployerAgricole {
 	private Ouvrier o;
 	private JoueurAgricole j1;
 	private DeployerAgricole a;
+	private Ocean o1;
 	
 
 	@Test
@@ -28,20 +29,21 @@ class TestDeployerAgricole {
 		this.p = new PlateauAgricole(20,10);
 		this.j1 = new JoueurAgricole("player1");
 		this.o= new Ouvrier(2);
+		this.a = new DeployerAgricole(p);
 		for(int x=0;x<this.p.getLargeur();x++){
 			for (int y=0;y<this.p.getHauteur();y++) {
 				if(!(this.p.getTuile(5, 3) instanceof Ocean)) {
-				this.p.getTuile(5, 3).setProprietaire(j1);
-				this.p.getTuile(5, 3).setPersonnage(o);
-				j1.setNbPersonnage(j1.getNbPersonnage()-1);
+					this.a.execute(j1, 5, 3, o);
 				assertEquals(j1,p.getTuile(5, 3).getProprietaire());
-				//assertEquals(34,j1.getNbPersonnage());
+				assertEquals(34,j1.getNbPersonnage());
 				
 				
 				}
+			
 				
 				
 			}
+			
 			
 			
 
@@ -54,6 +56,7 @@ class TestDeployerAgricole {
 			this.o= new Ouvrier(2);
 			this.a = new DeployerAgricole(p);
 			this.a.execute(j1, 1, 1, o);
+			
 			
 			
 			
