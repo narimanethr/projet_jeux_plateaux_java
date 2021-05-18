20da46cc37858f12d6648b303ceaa212bec8814e
Hicham Harra <hicham.harra.etu@univ-lille.fr>
2021-05-18 22:10:34 +0200

    rendu

--- project/src/GameAgricoleMain.java
+++ project/src/GameAgricoleMain.java
@@ -7,11 +7,14 @@ import plateaux.PlateauAgricole;
 
 
 public class GameAgricoleMain {
+	
 
 	public static void main(String[] args) throws RangeOutOfCapacityTileException, NoteFreeTileException, StockEmptyException {
+		String name1 =args[0];
+		String name2 =args[1];
 		PlateauAgricole p1 = new PlateauAgricole(10,5);
-		JoueurAgricole j1=new JoueurAgricole("j1");
-		JoueurAgricole j2=new JoueurAgricole("j2");
+		JoueurAgricole j1=new JoueurAgricole(name1);
+		JoueurAgricole j2=new JoueurAgricole(name2);
 		GameAgricole game =new GameAgricole(p1,j1,j2);
 		game.play();
 
--- project/src/GameGuerreMain.java
+++ project/src/GameGuerreMain.java
@@ -8,10 +8,11 @@ import plateaux.PlateauGuerre;
 public class GameGuerreMain {
 
 	public static void main(String[] args) throws RangeOutOfCapacityTileException, NoteFreeTileException, StockEmptyException {
-	
+		String name1 =args[0];
+		String name2 =args[1];
 		PlateauGuerre p1 = new PlateauGuerre(10,5);
-		JoueurGuerre j1=new JoueurGuerre("j1");
-		JoueurGuerre j2=new JoueurGuerre("j2");
+		JoueurGuerre j1=new JoueurGuerre(name1);
+		JoueurGuerre j2=new JoueurGuerre(name2);
 		System.out.println(j1.getNbOr());
 		System.out.println(j2.getNbOr());
 		GameGuerre game =new GameGuerre(p1,j1,j2);
