54e5f1efc1ec9d998c3511166d367949a3bdd115
harbane <said.harbane.etu@univ-lille.fr>
2021-05-18 11:52:38 +0200

    doc s

--- project/src/actions/DeployerAgricole.java
+++ project/src/actions/DeployerAgricole.java
@@ -10,9 +10,21 @@ import tuiles.*;
 
 
 public class DeployerAgricole extends DeployerGuerre implements Action{
+	/** modeliser l action Deploiement agricole 
+	 * 
+	 * @param p plateau sue lequel on deploie 
+	 */
 	public DeployerAgricole(Plateau p) {
 		super(p);
 	}
+	/**
+	 * execute l'action deploie
+	 * @param j joueur qui va deployer
+	 * @param y position de la tuile ou en deployer 
+	 * @param x position de la tuile ou en deployer 
+	 * @param o un ouvrier a deployer
+	 * @throws NoteFreeTileException
+	 */
 	public void execute(Joueur j,int y ,int x, Ouvrier o) throws NoteFreeTileException{
 		if ( this.plateau.getTuile(y,x).isFree()) {
 			this.plateau.getTuile(y,x).setProprietaire(j);		
